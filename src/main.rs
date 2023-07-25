a5b55a328a72850782c383149850d76ad0a0d479
mohuva13 <hussein30003@gmail.com>
2023-07-25 12:45:00 +0330

    Input len and generate password

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,14 @@ use generator::password_generator::gen_by_len as gen_by_len;
 mod generator;
 
 fn main() {
-    println!("{}", gen_by_len(10));
+    // input password length
+    let mut password_length = String::new();
+    println!("Input password length: ");
+    std::io::stdin().read_line(&mut password_length).expect("Failed to read line");
+    let password_length: i32 = password_length.trim().parse().expect("Please type a number!");
+
+    // generate password
+    println!("\n");
+    println!("Generated password:");
+    println!("{}", gen_by_len(password_length));
 }
