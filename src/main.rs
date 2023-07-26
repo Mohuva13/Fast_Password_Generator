1b4739e766191b5d47fe9ddb558bd51321962bb3
mohuva13 <hussein30003@gmail.com>
2023-07-26 12:03:40 +0330

    Use String instead direct fn

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use generator::password_generator::gen_by_len as gen_by_len;
 
+
 mod generator;
 
 fn main() {
@@ -11,8 +12,15 @@ fn main() {
     std::io::stdin().read_line(&mut password_length).expect("Failed to read line");
     let password_length: i32 = password_length.trim().parse().expect("Please type a number!");
 
+
+    // Password_string
+    let password_string = String::from(gen_by_len(password_length));
+
+
+
+
     // generate password
     println!("\n");
     println!("Generated password:");
-    println!("{}", gen_by_len(password_length));
+    println!("{}", password_string);
 }
